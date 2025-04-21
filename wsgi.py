8e1e8370f77cdba0bc99f3f24fc47cb1bfc87238
mike lipson <mlipson@hotmail.com>
2025-04-20 23:44:31 -0700

    042006

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,9 @@
+"""WSGI config for the Flask application."""
 
+import os
 from app import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    PORT = int(os.getenv("PORT", "8080"))
+    DEBUG = os.getenv("DEBUG", "False").lower() in ["true", "1", "t"]
+    app.run(port=PORT, debug=DEBUG)
