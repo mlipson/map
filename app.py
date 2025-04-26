9ed265dfc750b92023f3a2e308a65bea967d16f6
mike lipson <mlipson@hotmail.com>
2025-04-25 22:20:24 -0700

    042501

--- app.py
+++ app.py
@@ -438,5 +438,5 @@ def page_not_found(e):
     return render_template("404.html"), 404
 
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
